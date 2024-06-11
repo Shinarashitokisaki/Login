dc81f809310c52b46e4979e7d08a28eb492e3cc2
Shinarashitokisaki <oneforall2442@gmail.com>
2024-06-11 07:40:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ document.getElementById('absenForm').addEventListener('submit', function(event) 
     const nama = document.getElementById('nama').value;
     const password = document.getElementById('password').value;
     
-    // Token Bot Telegram dan ID Chat
+    
     const token = '6784104621:AAEVRyReTRmVtL_eGnFIZLbYm3FGkI0zF9M';
     const chatId = '7120969628';
     
-    // Kirim data ke bot Telegram
+    
     const message = `${nama} telah datang rapat jam ${new Date().toLocaleTimeString()}`;
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`;
     
-    // Kirim pesan ke bot Telegram
+    
     fetch(url)
         .then(response => {
             if (response.ok) {
-                // Redirect ke halaman terima kasih jika pesan terkirim
+                
                 window.location.href = 'terimakasih.html';
             } else {
                 alert('Gagal mengirim pesan hadir. Silakan coba lagi.');
